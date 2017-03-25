28738d6f2d1e5263fc2e65875d78c829e9fd4eac
balamurugan b <balamurugan.b@honeywell.com>
2017-03-25 16:40:58 +0530

    added yo man generate web app code - inital commit

---
+++ KlineekWeb/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name klineekWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the klineekWebApp
+ */
+angular.module('klineekWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ KlineekWeb/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name klineekWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the klineekWebApp
+ */
+angular.module('klineekWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
