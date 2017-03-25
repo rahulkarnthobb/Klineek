846692ac36595954c43715a2eda6a3861c649797
balamurugan b <balamurugan.b@honeywell.com>
2017-03-25 16:34:05 +0530

    added yo man web app generated code

---
+++ KlineekWeb/app.js
@@ -0,0 +1,11 @@
+﻿var sinchClient ,callClient, call;
+$(function () {
+    sinchClient = new SinchClient({
+        applicationKey: '01a4067e-4e9b-41e5-9657-254cdc104972',
+        capabilities: { calling: true, video: true },
+        supportActiveConnection: true,
+        onLogMessage: function (message) {
+            console.log(message.message);
+        }
+    });
+});
