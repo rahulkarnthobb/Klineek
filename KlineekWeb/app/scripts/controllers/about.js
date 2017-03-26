f3f397e03be6043d0e3782d16a407ee648f1bb84
balamurugan b <balamurugan.b@honeywell.com>
2017-03-26 10:55:20 +0530

    added about page

--- KlineekWeb/app/scripts/controllers/about.js
+++ KlineekWeb/app/scripts/controllers/about.js
@@ -1,17 +1,16 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name klineekWebApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the klineekWebApp
- */
-angular.module('klineekWebApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  /**
+   * @ngdoc function
+   * @name klineekWebApp.controller:AboutCtrl
+   * @description
+   * # AboutCtrl
+   * Controller of the klineekWebApp
+   */
+  angular.module('klineekWebApp')
+    .controller('AboutCtrl', function () {
+
+    });
+
+}());
